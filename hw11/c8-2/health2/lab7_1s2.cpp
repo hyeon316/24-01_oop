307fe8c8582ebedb01cce2e40fd80a79870ec1f9
hyeon316 <163807308+hyeon316@users.noreply.github.com>
2024-05-16 16:06:12 +0900

    Update lab7_1s2.cpp

--- hw11/c8-2/health2/lab7_1s2.cpp
+++ hw11/c8-2/health2/lab7_1s2.cpp
@@ -5,13 +5,17 @@ int main() {
 	
 	CHealth *students = new CHealth[5];		//5명 studnet
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 5; i++) {
 		students[i].measure(20 + rand()%4);	//나이를 입력하고 measure function 호출
 		(students + i)->run5min();			//student 객체마다 run5min function 실행
 	}
 
-	cout << students[1].cel << "C" << endl;
-	cout << students[1].faren << "F" << endl;
+	for (int i=0; i<5; i++){
+		cout << students[i].cel << "C, " << students[i].faren << "F" << endl;
+	}
+
+	//inline 되어 있는 member function과 default constructor를 helath2.cpp에 구현하였음에도 정상적으로
+	//작동되는것을 확인할 수 있다.
 
 	
 	delete [] students;  // delete objects
